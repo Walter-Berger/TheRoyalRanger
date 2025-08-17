bba697d0a222f2c8123a906f11b7d418cddbcdcf
Walter Berger <W.berger89@gmx.at>
2025-08-17 06:47:18 +0200

    player can now dash

--- code/player.py
+++ code/player.py
@@ -3,30 +3,38 @@ from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
+        # setup
         super().__init__(groups)
-        self.image = pygame.image.load(PLAYER_IMAGE_IDLE_DOWN).convert_alpha()
+        self.image = pygame.image.load('graphics/player/down/0.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
+        # movement
         self.direction = pygame.Vector2()
         self.speed = 500
+        self.dash_speed = self.speed * 3
+        self.dash_cooldown = -1.0
+        self.dash_duration = 0.2
+        self.dash_timer = 0
 
     def input(self):
-        keys = pygame.key.get_pressed()
-        # walking up and down
-        if keys[pygame.K_w]:    self.direction.y = -1
-        elif keys[pygame.K_s]:  self.direction.y = 1
-        else:                   self.direction.y = 0
-        # walking left and right
-        if keys[pygame.K_a]:    self.direction.x = -1
-        elif keys[pygame.K_d]:  self.direction.x = 1
-        else:                   self.direction.x = 0
+        keys = pygame.key.get_pressed()      
+        # movement input
+        self.direction.x = keys[pygame.K_d] - keys[pygame.K_a]
+        self.direction.y = keys[pygame.K_s] - keys[pygame.K_w]
+
+        # dash input
+        if keys[pygame.K_SPACE] and self.dash_timer <= self.dash_cooldown:
+            self.dash_timer = self.dash_duration
             
     def move(self, dt):
-        # if walking diagonal vector need to be normalized, otherwise player would move faster
+        # diagonal vector need to be normalized, otherwise player would move faster
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-
-        self.rect.topleft += self.direction * self.speed * dt 
+        
+        # determine current speed
+        current_speed = self.dash_speed if self.dash_timer > 0 else self.speed
+        self.rect.topleft += self.direction * current_speed * dt
+        self.dash_timer -= dt
 
     def update(self, dt):
         self.input()
--- code/settings.py
+++ code/settings.py
@@ -3,6 +3,3 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
 TILESIZE = 64
 FPS = 60
-
-# images
-PLAYER_IMAGE_IDLE_DOWN = 'graphics/player/down/0.png'
